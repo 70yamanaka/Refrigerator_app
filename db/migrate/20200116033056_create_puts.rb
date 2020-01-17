f4f126a7a94ac1050168dbe9d23350b88f68e4bc
70yamanaka <nao.ktm.12.25@icloud.com>
2020-01-17 14:39:38 +0900

    setting_migrate

--- db/migrate/20200116033056_create_puts.rb
+++ db/migrate/20200116033056_create_puts.rb
@@ -1,7 +1,12 @@
 class CreatePuts < ActiveRecord::Migration[5.2]
   def change
     create_table :puts do |t|
-
+      t.text :image, null: false
+      t.string :name, null: false
+      t.integer :bestby_date
+      t.integer :expiration_date, null: false
+      t.integer :category, null: false, default: 0
+      t.string :memo
       t.timestamps
     end
   end
