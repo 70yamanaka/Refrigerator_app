4994602807ec3f32ea530cee5a95e0d44d8f210b
70yamanaka <nao.ktm.12.25@icloud.com>
2020-01-21 13:44:38 +0900

    setting_create

--- app/assets/javascripts/puts_new_image.js
+++ app/assets/javascripts/puts_new_image.js
@@ -2,18 +2,22 @@ $(function(){
   //querySelectorでfile_fieldを取得
   var file_field = document.querySelector('input[type=file]')
   //fileが選択された時に発火するイベント
-  $('#img-file').change(function(){
-    //選択したfileのオブジェクトをpropで取得
-    var file = $('input[type="file"]').prop('files')[0];
-    //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-    var fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    //読み込みが完了すると、srcにfileのURLを格納
-    fileReader.onloadend = function() {
-      var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
-      //image_box__container要素の前にhtmlを差し込む
-      $('#image-box__container').before(html);
-    }
-  });
+  if($(".item-num-0").length == 1){
+    $('#img-file').change(function(){
+      //選択したfileのオブジェクトをpropで取得
+      var file = $('input[type="file"]').prop('files')[0];
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<div class="item-num-0">
+                    <img src="${src}" width="114" height="80">
+                   </div>`
+        //image_box__container要素の前にhtmlを差し込む
+        $('#image-box__container').before(html);
+      }
+    });
+  }
 });
