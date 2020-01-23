a4aa021849022603811e95d7d3fa15d05e82aa17
70yamanaka <nao.ktm.12.25@icloud.com>
2020-01-24 01:07:23 +0900

    setting_view

---
+++ app/assets/javascripts/password.js
@@ -0,0 +1,13 @@
+$(function() {
+  var password  = '#js-password';
+  var passcheck = '#js-passcheck';
+  
+  $(passcheck).change(function() {
+      if ($(this).prop('checked')) {
+          $(password).attr('type','text');
+      } else {
+          $(password).attr('type','password');
+      }
+  });
+});
+
