e034d1b1d2c47534e3bde14ada296616ee99585a
70yamanaka <nao.ktm.12.25@icloud.com>
2020-01-28 18:30:37 +0900

    setting_js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -25,8 +25,8 @@ $(function() {
   search_list.append(html);
   }
 
-  $(".search-input").on("keyup", function() {
-    var input = $(".search-input").val();
+  $(".in__main__header__search-form__search-input").on("keyup", function() {
+    var input = $(".in__main__header__search-form__search-input").val();
     $.ajax({
       type: 'GET',
       url: '/puts/search',
