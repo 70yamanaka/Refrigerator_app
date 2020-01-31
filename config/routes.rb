3627f82c3bb88721ea0c2b58f0c804d4c9f7495a
70yamanaka <nao.ktm.12.25@icloud.com>
2020-01-31 18:21:23 +0900

    setting_route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,28 @@ Rails.application.routes.draw do
     collection do
       get 'top'
       get 'search'
+      get 'refrigerator_meat'
+      get 'refrigerator_vegetable'
+      get 'refrigerator_fish'
+      get 'refrigerator_fruit'
+      get 'refrigerator_bread'
+      get 'refrigerator_rice'
+      get 'refrigerator_milk'
+      get 'refrigerator_desert'
+      get 'refrigerator_drink'
+      get 'refrigerator_spice'
+      get 'refrigerator_other'
+      get 'freezer_meat'
+      get 'freezer_vegetable'
+      get 'freezer_fish'
+      get 'freezer_fruit'
+      get 'freezer_bread'
+      get 'freezer_icefood'
+      get 'freezer_milk'
+      get 'freezer_desert'
+      get 'freezer_drink'
+      get 'freezer_icespice'
+      get 'freezer_other'
     end
   end
 end
