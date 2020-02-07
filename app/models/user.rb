6627bab3794e30018cc420b3eaeaeb358c2f5205
70yamanaka <nao.ktm.12.25@icloud.com>
2020-02-07 17:21:37 +0900

    setting_nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,128}+\z/i
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: {maximum: 10}
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :password,              presence: true, length: {minimum: 6, maximum: 128},    format: { with: PASSWORD_VALIDATION }
 end
