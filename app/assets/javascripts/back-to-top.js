7ccb6c9ff75f8c29daa5885d80742cf9f7970f4e
70yamanaka <nao.ktm.12.25@icloud.com>
2020-02-21 18:48:53 +0900

    setting_color

--- app/assets/javascripts/back-to-top.js
+++ app/assets/javascripts/back-to-top.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function() {
-      if($(this).scrollTop() > 100) { // 100pxスクロールしていたら上に戻るボタンを表示
+      if($(this).scrollTop() > 100) { 
           $(".in__main__header__outside").fadeIn(); 
       } else {
           $(".in__main__header__outside").fadeOut();
       }
   });
   $(".in__main__header__outside").click(function() {
-      $("body,html").animate({scrollTop:0},800); // 800msかけて上に戻る
+      $("body,html").animate({scrollTop:0},1000); 
   });
 });
