bb5a95b61302228ac01e1d752badc973226c06d2
70yamanaka <nao.ktm.12.25@icloud.com>
2020-02-21 18:59:32 +0900

    setting_color

--- app/assets/javascripts/back-to-top.js
+++ app/assets/javascripts/back-to-top.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function() {
       if($(this).scrollTop() > 100) { 
-          $(".in__main__header__outside").fadeIn(); 
+          $(".in__main__header__back-to-top").fadeIn(); 
       } else {
-          $(".in__main__header__outside").fadeOut();
+          $(".in__main__header__back-to-top").fadeOut();
       }
   });
-  $(".in__main__header__outside").click(function() {
+  $(".in__main__header__back-to-top").click(function() {
       $("body,html").animate({scrollTop:0},1000); 
   });
 });
